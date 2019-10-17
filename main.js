5b45054cf1d02da1bc46be67523f7c16c85d87b9
Binaya Shrestha <BinayaShree@gmail.com>
2019-10-17 11:33:24 -0500

    cleaned up main.js code + added in main.css to modify canvas parameters for pixel art
    - code is now in blocks
    - all blocks have discription comments
    - main.css added in with canvas parameters

--- main.js
+++ main.js
@@ -1,52 +1,57 @@
 
-
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-
-canvas.width = 800;
-canvas.height = 400;
-
-//get DPI
-let dpi = window.devicePixelRatio;
-function fix_dpi() {
-    //get CSS height
-    //the + prefix casts it to an integer
-    //the slice method gets rid of "px"
-    let style_height = +getComputedStyle(canvas).getPropertyValue("height").slice(0, -2);
-    //get CSS width
-    let style_width = +getComputedStyle(canvas).getPropertyValue("width").slice(0, -2);
-    //scale the canvas
-    canvas.setAttribute('height', style_height * dpi);
-    canvas.setAttribute('width', style_width * dpi);
+//---- Init. Vars ----//
+{
+    var canvas = document.getElementById("myCanvas");
+    var ctx = canvas.getContext("2d");
+    var canvasHeight = 400;
+    var canvasWidth = 800;
 }
 
-var img = new Image();
-img.src = "/_assets/Player.png";
-
-var s = new Sprite(5, 5, 10, 10, "rgb(0, 255, 0)");
-var s2 = new Sprite(img.width/2, img.height/2, undefined, undefined, undefined, img);
-
-var o = new Object(s, 200, 100);
-var o2 = new Object(s2, 200, 100);
-
-
-function update(progress) {
+//---- Setup Canvas ----//
+{
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
 }
 
-function draw(ctx) {
-    o2.draw(ctx);
-    o.draw(ctx);
+
+//---- Test Code ----//
+{
+    var img = new Image();
+    img.src = "/_assets/Player.png";
+    
+    var s = new Sprite(2.5, 2.5, 5, 5, "rgb(0, 255, 0)");
+    var s2 = new Sprite(img.width/2, img.height/2, undefined, undefined, undefined, img);
+    
+    var o = new Object(s, 200, 100);
+    var o2 = new Object(s2, 200, 100);
 }
 
-function loop(timestamp) {
-    var progress = timestamp - lastRender
 
-    update(progress)
-    draw(ctx)
 
-    lastRender = timestamp
-    window.requestAnimationFrame(loop)
+//---- Game Loop ----//
+{
+    function update(progress) {
+    }
+    
+    function draw(ctx) {
+        o2.draw(ctx);
+        o.draw(ctx);
+    }
+    
+    function loop(timestamp) {
+        var progress = timestamp - lastRender;
+    
+        update(progress);
+        draw(ctx);
+    
+        lastRender = timestamp;
+        window.requestAnimationFrame(loop);
+    }
 }
 
-var lastRender = 0
-window.requestAnimationFrame(loop)
+
+//---- Start Loop ----//
+{
+    var lastRender = 0;
+    window.requestAnimationFrame(loop);
+}
